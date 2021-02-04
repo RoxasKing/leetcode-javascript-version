31f1cbd1fdc2cd51fc7bd532d530a865763d2b5a
RoxasKing <tekkenfansking@gmail.com>
2021-02-04 09:46:22 +0800

    add 0424 0480 0643 0778 0839 0888 1631, update 1046

---
+++ 0839_similar-string-groups/similar-string-groups.test.js
@@ -0,0 +1,16 @@
+import { numSimilarGroups } from './similar-string-groups.js'
+import assert from 'assert'
+
+describe('numSimilarGroups()', () => {
+  let tests = [
+    { args: ["tars", "rats", "arts", "star"], expected: 2 },
+    { args: ["omv", "ovm"], expected: 1 },
+    { args: ["abc", "abc"], expected: 1 },
+  ]
+
+  tests.forEach((test) => {
+    it(`numSimilarGroups(${test.args}) === ${test.expected}`, () => {
+      assert.deepStrictEqual(numSimilarGroups(test.args), test.expected)
+    })
+  })
+})
